bd4e1e819ad5e106b9dd7dae2cb035d6af0610b4
sushmita12-hub <sushmitadutta11011989@gmail.com>
2019-09-25 09:32:51 -0400

    add new testcases

---
+++ MetLife/src/test/java/testPages/VerifyPageTitleTest.java
@@ -0,0 +1,18 @@
+package testPages;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyPageTitleTest extends CommonAPI {
+    WebDriver driver;
+    @Test
+    public void VerifyPageTitleTest() {
+        String title = driver.getTitle();
+        System.out.println("the page title is:" + title);
+        Assert.assertEquals(title,"Evolution");
+
+
+    }
+}
